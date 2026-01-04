fa1c2a5a2d0fcf27355db302faefe100b04ccbe5
HAIDAR ALFARISI <haidarhabibi178@gmail.com>
2026-01-04 22:10:08 +0700

    week7-koleksi keranjang

--- praktikum/week7-koleksi-keranjang/src/main/java/com/upb/agripos/Maincart.java
+++ praktikum/week7-koleksi-keranjang/src/main/java/com/upb/agripos/Maincart.java
@@ -3,9 +3,9 @@ package com.upb.agripos;
 
 public class Maincart {
     public static void main(String[] args) {
-        System.out.println("Hello, I am [Nama]-[NIM] (Week7)");
+        System.out.println("Hello, I am Haidar Habibi Al Farisi-240202862 (Week7)");
 
-        Product p1 = new Product("P01", "Beras", 50000);
+        Product p1 = new Product("P01", "Beras", 50000, 0);
         Product p2 = new Product("P02", "Pupuk", 30000);
         
 
