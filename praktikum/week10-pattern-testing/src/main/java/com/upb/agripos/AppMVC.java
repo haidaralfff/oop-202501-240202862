27ba073e78098213a3ce18071d8d0348517dcbf6
HAIDAR ALFARISI <haidarhabibi178@gmail.com>
2026-01-11 13:46:17 +0700

    pattern testing and implementation MVC

--- praktikum/week10-pattern-testing/src/main/java/com/upb/agripos/AppMVC.java
+++ praktikum/week10-pattern-testing/src/main/java/com/upb/agripos/AppMVC.java
@@ -3,13 +3,22 @@ package com.upb.agripos;
 import com.upb.agripos.model.Product;
 import com.upb.agripos.view.ConsoleView;
 import com.upb.agripos.controller.ProductController;
+import com.upb.agripos.config.DatabaseConnection;
 
 public class AppMVC {
     public static void main(String[] args) {
-        System.out.println("Hello, I am [Nama]-[NIM] (Week10)");
+
+        System.out.println("Hello, Haidar Habibi Al Farisi-240202862 (Week10)");
+
+        // Singleton test
+        DatabaseConnection db1 = DatabaseConnection.getInstance();
+        db1.connect();
+
+        // MVC
         Product product = new Product("P01", "Pupuk Organik");
         ConsoleView view = new ConsoleView();
         ProductController controller = new ProductController(product, view);
+
         controller.showProduct();
     }
 }
